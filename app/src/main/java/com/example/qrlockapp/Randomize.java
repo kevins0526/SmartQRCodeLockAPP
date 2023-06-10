864c416645569c83cd8d893a2eb57cb7ef8c4c58
陌 <kevins0526s@gmail.com>
2023-06-10 23:00:42 +0800

    last ver

--- app/src/main/java/com/example/qrlockapp/Randomize.java
+++ app/src/main/java/com/example/qrlockapp/Randomize.java
@@ -1,12 +1,11 @@
 package com.example.qrlockapp;
 import java.util.Random;
 public class Randomize {
-    Random random=new Random();
-    long number=1000000000000000L + (long)(random.nextDouble() * 9000000000000000L);
-    String randomString = Long.toString(number);
     public static long IV(){
+        //储存現在時間的微秒 作為Ranodm 的種子
+        long currentTimestamp = System.currentTimeMillis();
         try {
-            Random random=new Random();
+            Random random=new Random(currentTimestamp);
             long number=1000000000000000L + (long)(random.nextDouble() * 9000000000000000L);
             return number;
         } catch (Exception ex) {
